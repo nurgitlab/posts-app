164b5affa7b26a0f927d3f15db801e1d8ff16ef5
nurgitlab <nurbek.math@gmail.com>
2021-07-25 03:11:41 +0600

    bootstrap added

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import Post from './Post'
 
-export default ({posts}) => {
+const Posts = ({posts}) => {
     if (!posts.length){
         return <p className="text-center">Постов пока нет</p>
     }
     return posts.map(post => <Post post={post} key={post}/>)
 }
+
+export default connect(null, null)(Posts)
