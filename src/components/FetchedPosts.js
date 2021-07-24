3f3d46c865da9a95733cd8f40401a0f39a11f69b
nurgitlab <nurbek.math@gmail.com>
2021-07-25 02:18:23 +0600

    project created

---
+++ src/components/FetchedPosts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Post from './Post'
+
+export default ({posts}) => {
+
+    if (!posts.length){
+        return <button className="btn btn-primary">Загрузить посты</button>
+    }
+
+    return posts.map(post => <Post post={post} key={post}/>)
+    
+}
