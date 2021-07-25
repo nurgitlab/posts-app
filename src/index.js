db6bb3cb25e535489412f14c1fc87ad1d881e208
nurgitlab <nurbek.math@gmail.com>
2021-07-25 23:17:46 +0600

    crush in input fixed

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import {compose, createStore} from 'redux'
+import {compose, createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import {rootReducer} from './redux/rootReducer'
+import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer, compose(
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-))
+
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const app = (
   <Provider store={store}>
--- src/redux/types.js
+++ src/redux/types.js
@@ -1,1 +1,4 @@
 export const CREATE_POST = 'POST/CREATE_POST'
+export const FETCH_POSTS = 'POST/FETCH_POSTS'
+//export const CREATE_POST = 'POST/CREATE_POST'
+//export const CREATE_POST = 'POST/CREATE_POST'
